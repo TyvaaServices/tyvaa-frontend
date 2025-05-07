1972c2093a7d0170f16bf93f1ef5e0e836cbc71a
traorecheikh <atidiane741@gmail.com>
2025-05-07 16:33:46 +0000

    refactor: adjust colors and text styles for better theme consistency and improve TypingIndicator layout n border.

--- lib/app/widgets/typing_indicator.dart
+++ lib/app/widgets/typing_indicator.dart
@@ -1,7 +1,6 @@
-
-
 import 'package:flutter/material.dart';
 import 'package:flutter_spinkit/flutter_spinkit.dart';
+
 class TypingIndicator extends StatelessWidget {
   final Color botColor;
 
@@ -10,22 +9,21 @@ class TypingIndicator extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Align(
-      alignment: Alignment.bottomLeft, // Aligns the indicator to the bottom-left of the screen
+      alignment: Alignment.bottomLeft,
+      // Aligns the indicator to the bottom-left of the screen
       child: Container(
         width: 80,
-        margin: EdgeInsets.symmetric(vertical: 8), // Adds margin around the indicator
+        margin: EdgeInsets.symmetric(vertical: 8),
+        // Adds margin around the indicator
         padding: EdgeInsets.all(12),
         decoration: BoxDecoration(
           color: botColor.withOpacity(0.1),
           borderRadius: BorderRadius.circular(20),
-          border: Border.all(
-            color: botColor.withOpacity(0.3),
-            width: 1.5,
-          ),
+          border: Border.all(color: botColor.withOpacity(0.3), width: 1.5),
         ),
         child: SpinKitThreeBounce(
           color: botColor,
-          size: 16,  // The size of the typing indicator
+          size: 16, // The size of the typing indicator
         ),
       ),
     );
