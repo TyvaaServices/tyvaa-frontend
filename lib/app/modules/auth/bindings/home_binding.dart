3704846dbc5bf76be397607f0cda08d6b3f03bb1
traorecheikh <atidiane741@gmail.com>
2025-05-08 15:09:50 +0000

    feat: add OTP verification controller binding in authentication

--- lib/app/modules/auth/bindings/home_binding.dart
+++ lib/app/modules/auth/bindings/home_binding.dart
@@ -1,11 +1,17 @@
 import 'package:get/get.dart';
+import 'package:passenger_tyvaa/app/modules/auth/controllers/otp_controller.dart';
 
 import '../controllers/login_controller.dart';
 
-class Authentication extends Bindings {
+class AuthenticationBinding extends Bindings {
   @override
   void dependencies() {
     Get.lazyPut<LoginController>(fenix: true, () => LoginController());
     //TODO : register controller here
+
+    Get.lazyPut<OtpVerificationController>(
+      fenix: true,
+      () => OtpVerificationController(),
+    );
   }
 }
