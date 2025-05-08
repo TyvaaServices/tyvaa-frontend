7fed251c50bf12d144edd7db0a629610bdc32e08
Houleymatou DIALLO <dhouleymatou150@gmail.com>
2025-05-08 16:49:17 +0100

    feat: rename home_binding to auth_binding and add register controller

--- lib/app/modules/auth/bindings/auth_binding.dart
+++ lib/app/modules/auth/bindings/auth_binding.dart
@@ -1,11 +1,13 @@
 import 'package:get/get.dart';
 
 import '../controllers/login_controller.dart';
+import '../controllers/register_controller.dart';
 
 class Authentication extends Bindings {
   @override
   void dependencies() {
     Get.lazyPut<LoginController>(fenix: true, () => LoginController());
+    Get.lazyPut<RegisterController>(fenix: true, () => RegisterController());
     //TODO : register controller here
   }
 }
