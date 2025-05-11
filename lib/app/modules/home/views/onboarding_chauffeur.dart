13c552507bd1f10019fd9aaa71a807b1d42d2fea
Houleymatou DIALLO <dhouleymatou150@gmail.com>
2025-05-11 18:52:56 +0100

    fix: navigate to PUBLISH_TRAJET route on onboarding completion
    This commit changes the behavior on completing the onboarding flow.
    Instead of navigating back, the user is now directed to the `Routes.PUBLISH_TRAJET` page.
    A `PublishTrajetController` is also instantiated within the onboarding view, although it is not currently used.

--- lib/app/modules/home/views/onboarding_chauffeur.dart
+++ lib/app/modules/home/views/onboarding_chauffeur.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:passenger_tyvaa/app/modules/trajet/controllers/publish_trajet_controller.dart';
+import 'package:passenger_tyvaa/app/routes/app_pages.dart';
 
 import '../../../themes/tyvaa_theme.dart';
 
@@ -16,6 +18,8 @@ class PublierTrajetOnboarding extends StatefulWidget {
 class _PublierTrajetOnboardingState extends State<PublierTrajetOnboarding>
     with TickerProviderStateMixin {
   final PageController _pageController = PageController();
+  final PublishTrajetController _publishTrajetController =
+      PublishTrajetController();
   final RxInt _currentPage = 0.obs;
 
   late AnimationController _animationController;
@@ -262,7 +266,7 @@ class _PublierTrajetOnboardingState extends State<PublierTrajetOnboarding>
                               if (widget.onComplete != null) {
                                 widget.onComplete!();
                               } else {
-                                Get.back();
+                                Get.toNamed(Routes.PUBLISH_TRAJET);
                               }
                             }
                           },
