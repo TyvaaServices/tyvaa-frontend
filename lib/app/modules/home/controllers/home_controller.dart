79cdd789dd2c25ab0ec2598f7a1bb5f3d24a0c0a
traorecheikh <atidiane741@gmail.com>
2025-05-14 18:37:07 +0000

    feat: Implement authentication check in onInit to redirect unauthenticated users to login

--- lib/app/modules/home/controllers/home_controller.dart
+++ lib/app/modules/home/controllers/home_controller.dart
@@ -2,6 +2,7 @@ import 'dart:async';
 
 import 'package:confetti/confetti.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_secure_storage/flutter_secure_storage.dart';
 import 'package:geocoding/geocoding.dart';
 import 'package:geolocator/geolocator.dart';
 import 'package:get/get.dart';
@@ -50,7 +51,12 @@ class HomeController extends GetxController {
   //   super.onInit();
   // }
   @override
-  void onInit() {
+  Future<void> onInit() async {
+    FlutterSecureStorage storage = const FlutterSecureStorage();
+    var value = await storage.read(key: 'auth_token');
+    if (value == null) {
+      Get.offAllNamed('/login');
+    }
     _determinePosition();
     super.onInit();
     confettiController = ConfettiController(
