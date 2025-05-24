8835c7d1f69ded88231288ae29905edf2e9c12b6
traorecheikh <atidiane741@gmail.com>
2025-05-24 17:45:23 +0000

    feat: Remove unused LongRide adapter registration from service_initializer

--- lib/app/core/service_initializer.dart
+++ lib/app/core/service_initializer.dart
@@ -8,7 +8,6 @@ import 'package:passenger_tyvaa/app/services/connectivity_service.dart';
 import 'package:passenger_tyvaa/app/services/notification_service.dart';
 import 'package:passenger_tyvaa/app/services/permission_service.dart';
 import 'package:passenger_tyvaa/app/services/synchronization_service.dart';
-import 'package:passenger_tyvaa/domain/entities/long_ride.dart';
 import 'package:passenger_tyvaa/domain/entities/user.dart';
 import 'package:passenger_tyvaa/firebase_options.dart';
 
@@ -24,9 +23,7 @@ class ServiceInitializer {
 
     await Hive.initFlutter();
     Hive.registerAdapter(UserAdapter());
-    Hive.registerAdapter(LongRideAdapter());
     await Hive.openBox<User>('users');
-    await Hive.openBox<LongRide>('long_rides');
 
     Get.put(ConnectivityController(), permanent: true);
     Get.put(ApiClient(), permanent: true);
--- lib/app/modules/trajet/bindings/publish_trajet_binding.dart
+++
@@ -1,9 +0,0 @@
-import 'package:get/get.dart';
-import 'package:passenger_tyvaa/app/modules/trajet/controllers/publish_trajet_controller.dart';
-
-class PublishTrajetBinding extends Bindings {
-  @override
-  void dependencies() {
-    Get.lazyPut<PublishTrajetController>(() => PublishTrajetController());
-  }
-}
