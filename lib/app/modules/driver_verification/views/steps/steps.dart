dbca26013b89a98eaf3b15b5c089d826dccda33c
traorecheikh <atidiane741@gmail.com>
2025-06-03 03:19:22 +0000

    feat: Add new steps for driver verification process and update dependencies

---
+++ lib/app/modules/driver_verification/views/steps/steps.dart
@@ -0,0 +1,6 @@
+export 'welcome_step.dart';
+export 'driver_license_step.dart';
+export 'carte_grise_step.dart';
+export 'identity_step.dart';
+export 'review_step.dart';
+export 'success_step.dart';
